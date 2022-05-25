7e56e90137eebbb93c217b071df729d4c24483a0
raju2411 <104960231+raju2411@users.noreply.github.com>
2022-05-25 09:43:37 +0530

    Add files via upload

---
+++ Light/app.js
@@ -0,0 +1,19 @@
+// for on button
+
+document.querySelector(".on").addEventListener("click",function(){
+
+    document.querySelector(".bulbimage").src="on.gif"
+
+
+
+})
+
+
+// for off button
+
+
+document.querySelector(".off").addEventListener("click",function(){
+
+    document.querySelector(".bulbimage").src="off.gif"
+
+})
